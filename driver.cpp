fb770d7efaf569c6e752c882568b7abbb563933e
Colby Gram <123613082+colbygram@users.noreply.github.com>
2025-07-04 13:07:13 -0700

    Parse Function
    simple parse function that will determine which command is inputted and if the command is valid

--- driver.cpp
+++ driver.cpp
@@ -1,3 +1,32 @@
-int main() {
-  return 0;
+//Authors: William Chuter-Davies, Colby Gramelspacher
+#include <iostream>
+#include <algorithm>
+
+
+//Will parse commands. Return false if command is unrecognized
+bool ParseCommand(const std::string command) {
+    //Converts all string characters to lowercase
+    std::string temp = command;
+    std::transform(temp.begin(), temp.end(), temp.begin(), std::tolower);
+
+    if (temp == "add") {
+        //Check and add VIN to list
+        return true;
+    }
+    else if (temp == "remove") {
+        //Check and remove VIN from list
+        return true;
+    }
+    else if (temp == "legal") {
+        //Check if parked legally or illegally
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    std::string command = "remove";
+    if (!ParseCommand(command)) std::cout << "Error: Unrecognized Command!\n";
+    return 0;
 }
