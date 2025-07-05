8ab92c2e59be5c9a76518ed3c551590b11d0dcc3
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-07-04 21:05:53 -0700

    Update driver.cpp
    Added additional context to header comments. Replaced ParseCommand with process_terminal_input. Finished writing ParseCommand functionality in process_terminal_input. Wrote load_bcn_to_lp. Instantiated ifs, bcn_to_lp, and lp_to_ps in main. Replaced all terminators with appropriate macros defined in stdexcept. Fixed error in std::transform on current line 14. Setup program for exception handling.

--- driver.cpp
+++ driver.cpp
@@ -1,32 +1,80 @@
-//Authors: William Chuter-Davies, Colby Gramelspacher
-#include <iostream>
+// written by William Chuter-Davies & Colby Gramelspacher
+// barcode number (bcn), license plate (lp), parking status (ps)
+
 #include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <stdexcept>
 
-
-//Will parse commands. Return false if command is unrecognized
-bool ParseCommand(const std::string command) {
-    //Converts all string characters to lowercase
-    std::string temp = command;
-    std::transform(temp.begin(), temp.end(), temp.begin(), std::tolower);
-
-    if (temp == "add") {
-        //Check and add VIN to list
-        return true;
+// process text input from terminal
+void process_terminal_input(std::string line, std::map<std::string, std::string>& bcn_to_lp, std::map<std::string, bool>& lp_to_ps) {
+    // set all chars in 'line' to lowercase
+    std::transform(line.begin(), line.end(), line.begin(), [](unsigned char c) {return std::tolower(c);});
+    unsigned int delimeter_pos = line.find_first_of(' ');
+    std::string command = line.substr(0, delimeter_pos);
+    std::string bcn = line.substr(delimeter_pos);
+    // throw on unrecognized barcode number
+    if(bcn_to_lp.find(bcn) == bcn_to_lp.end()) {
+        throw std::runtime_error("error: unrecognized barcode number");
     }
-    else if (temp == "remove") {
-        //Check and remove VIN from list
-        return true;
+    if(command == "park") {
+        // set parking status to true
+        lp_to_ps[bcn_to_lp[bcn]] = true;
+    } else if(command == "unpark") {
+        // set parking status to false
+        lp_to_ps[bcn_to_lp[bcn]] = false;
+    } else if(command == "status") {
+        // if parking status is true, return "parked". otherwise, return "unparked"
+        if(lp_to_ps[bcn_to_lp[bcn]]) {
+            std::cout << "parked" << '\n';
+        } else {
+            std::cout << "unparked" << '\n';
+        }
+    } else {
+        // throw on unrecognized command
+        throw std::runtime_error("error: unrecognized command");
     }
-    else if (temp == "legal") {
-        //Check if parked legally or illegally
-        return true;
-    }
-    return false;
+    return;
 }
 
-int main()
-{
-    std::string command = "remove";
-    if (!ParseCommand(command)) std::cout << "Error: Unrecognized Command!\n";
-    return 0;
+// load bcn_to_lp map with reference data
+void load_bcn_to_lp(std::map<std::string, std::string>& bcn_to_lp, std::ifstream& ifs) {
+    std::string line, bcn, lp;
+    std::getline(ifs, line);
+    while(std::getline(ifs, line)) {
+        unsigned int delimeter_pos = line.find_first_of(',');
+        bcn = line.substr(0, delimeter_pos);
+        lp = line.substr(delimeter_pos + 1);
+        bcn_to_lp[bcn] = lp;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {   
+        std::cerr << "usage: ./driver *.csv" << '\n';
+        return EXIT_FAILURE;
+    }
+    std::ifstream ifs(argv[1]);
+    if(!ifs) {
+        std::cerr << "error: ifs failure" << '\n';
+        return EXIT_FAILURE;
+    }
+    // instantiate barcode number to license plate map
+    std::map<std::string, std::string> bcn_to_lp;
+    load_bcn_to_lp(bcn_to_lp, ifs);
+    // instantiate license plate to parking status map
+    std::map<std::string, bool> lp_to_ps;
+    
+
+
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
 }
